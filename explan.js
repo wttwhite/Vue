315661de00752040106dc4a1b3012062c8afa389
王婷婷8 <wangtingting8@hikvision.com>
2017-07-18 17:53:08 +0800

    v-for+bind+demo

---
+++ explan.js
@@ -0,0 +1,26 @@
+////Auto Close Tag  自动补全标签
+////Auto Rename Tag 自动重命名标签
+////Beautify
+////Debugger for Chrome 在 Code 中调试
+//ECMAScript Quotes Transformer 有时字符串写了一半想转成模板字符串, 或者想从模板字符串转为普通字符串的时候比较顺手.
+////ESLint  检查Javascript编程时的语法错误
+////Git Lens
+//Mithril Emmet
+////open-in-browser
+////Quokka.js Quokka 是一个调试工具插件，能够根据你正在编写的代码提供实时反馈。它易于配置，并能够预览变量的函数和计算值结果。另外，在使用 JSX 或 TypeScript 项目中，它能够开箱即用。
+//Sensitive Replace
+////Settings Sync 设置同步,可以同步配置插件等到 gist.
+    //上传 shift+alt+U  token
+    //下载 shift+alt+D   gist id
+//Sublime Text Keymap
+////Vetur
+    //语法错误检查，包括 CSS/SCSS/LESS/Javascript/TypeScript
+    //语法高亮，包括 html/jade/pug css/sass/scss/less/stylus js/ts
+    //emmet 支持
+    //代码自动补全（目前还是初级阶段），包括 HTML/CSS/SCSS/LESS/JavaScript/TypeScript
+    //配合 ESLint 插件食用效果更佳。
+////vscode-faker  ctrl+shift+p
+    //能够帮你快速的插入用例数据。Faker 可以随机生成姓名、地址、图像、电话号码
+////vscode-icons
+////Vue 2 Snippets 片段补全工具,语法高亮
+////Code Spellchecker 拼写检查, 速度相对快.
